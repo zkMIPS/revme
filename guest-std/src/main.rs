0d75646a9ebbc08276781dfb4d6eab717425f2e6
Jingping Yin <yinjingping2022@gmail.com>
2025-03-27 10:09:25 +0800

    feat: add revme guest (#16)
    * feat: add revme guest
    
    * chore: remove zkm2
    
    * style: rename guest-lib
    
    * chore: update readme
    
    * style: refactor guest
    
    * Revert "style: refactor guest"
    
    This reverts commit 42771f175abe8c65fc883d347d7fdc5941c06a28.

---
+++ guest-std/src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+extern crate alloc;
+use alloc::collections::BTreeMap;
+
+use guest::verify_revm_tx;
+use models::TestUnit;
+
+pub fn main() {
+    let manifest_path = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let json_path =
+        env::var("JSON_PATH").unwrap_or(format!("{}/test-vectors/test.json", manifest_path));
+    let mut f = File::open(json_path).unwrap();
+    let mut data = vec![];
+    f.read_to_end(&mut data).unwrap();
+
+    let suite: BTreeMap<String, TestUnit> = serde_json::from_slice(&data).map_err(|e| e).unwrap();
+    let encoded = serde_cbor::to_vec(&suite).unwrap();
+
+    assert!(verify_revm_tx(&encoded));
+
+    println!("finish");
+}
